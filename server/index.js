cfacc99483a1c77f0908ad1f566379ac60e1a2ea
MCSmorenberg <mcsmorenberg@gmail.com>
2018-02-08 16:43:20 +0100

    first request wit express

--- server/index.js
+++ server/index.js
@@ -3,7 +3,12 @@ const express = require('express');
 // set up express app
 const app = express();
 
+app.get('/api', function(req, res){
+  console.log('GET request');
+  res.send({ name: 'TEST'});
+});
+
 // listen for requests
-app.listen(process.env || 4000, function(){
+app.listen(process.env.port || 4000, function(){
   console.log('Now listen for requests');
 });
